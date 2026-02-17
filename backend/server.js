961a66a84cd405ae2628282108b8cf1f704cfe0f
vinothkumarm1845 <m.vinoth457492@gmail.com>
2026-02-17 20:06:21 +0530

    pushing final changes

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,7 @@ app.use((req, res, next)=>{
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
     next();
 })
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://vinothkumar-m18.github.io'
-  ],
-  credentials: true   
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
